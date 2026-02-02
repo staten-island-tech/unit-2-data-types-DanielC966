0dc56c9379e8f53a3fc9d8c120e7b31144c9ddb0
DanielC966 <danielc966@nycstudents.net>
2026-02-02 09:25:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,8 @@ print(str(wordCounter(sentence)) + " words")
 
 #Challenge 2: Mad Libs
 
-coolsentence = ""
+firstAdj = input("Input an adjective: ")
+secondNoun = input("Input a character: ")
+thirdVerb = input("Input a verb (-ing): ")
+fourthVerb = input("Input another verb (past tense): ")
+print(f"It was a {firstAdj} day in the park when {secondNoun} unexpectedly caught his dad {thirdVerb} the dog. Then, {secondNoun} {fourthVerb} ")
