9e9a0fb7f2cb7fab2d65e042e60ca3d2a9e25a04
DanielC966 <danielc966@nycstudents.net>
2026-02-02 09:18:35 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+#Challenge 1: Word Count
+
+"""
+sentence = input("Input a sentence: ")
+
+def wordCounter(sentence):
+    s = sentence
+    wordList = s.split()
+    counted = 0
+    for i in wordList:
+        counted +=1
+    
+    return counted
+
+print(str(wordCounter(sentence)) + " words")
+"""
+
+#Challenge 2: Mad Libs
+
+coolsentence = ""
