4efa7877d9fba55176e7893f9f3f7d8a9d8217b2
DanielC966 <danielc966@nycstudents.net>
2026-02-03 09:26:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,52 @@ print(str(wordCounter(sentence)) + " words")
 """
 
 #Challenge 2: Mad Libs
-
+"""
 firstAdj = input("Input an adjective: ")
 secondNoun = input("Input a character: ")
 thirdVerb = input("Input a verb (-ing): ")
 fourthVerb = input("Input another verb (past tense): ")
-print(f"It was a {firstAdj} day in the park when {secondNoun} unexpectedly caught his dad {thirdVerb} the dog. Then, {secondNoun} {fourthVerb} ")
+print(f"It was a {firstAdj} day in the park when {secondNoun} unexpectedly caught his dad {thirdVerb} the dog. Then, {secondNoun} {fourthVerb} into the pond.")
+"""
+
+#Problem 2
+"""
+def discount(age, isResident, isMember):
+    if age < 12 or age >= 65:
+        print("dicoiunt givne")
+    elif isResident == True or isMember == True:
+        print("discount granted")
+    else:
+        print("NOOOOOO discount")
+
+discount(26, False, True)
+"""
+
+#Challenge: Odd or Even
+
+"""
+def checkNumber(x):
+    if x % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+checkNumber(35)
+
+"""
+
+#Challenge: Bill Tip
+
+def determineTip(billValue, service):
+    if service == "bad":
+        print(billValue)
+    elif service == "okay":
+        billValue = billValue + (billValue * 0.15)
+        print(billValue)
+    elif service == "good":
+        billValue = billValue + (billValue * 0.2)
+        print(billValue)
+    elif service == "great":
+        billValue = billValue + (billValue * 0.25)
+
+determineTip(15, "great")
